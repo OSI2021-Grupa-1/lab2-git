80ea92404b49a4b39fa5cc9bec65ec223cc1e9f4
Nikola Jelic <jelicnikola37@gmail.com>
2021-11-01 12:28:05 +0100

    main calc

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,38 @@ double add(double const a, double const b) {
 	return a + b;
 }
 int main() {
+	bool running = true;
+	std::cout << "Kalkulator:" << std::endl;
+	while (running) {
+		double a, b;
+		std::cout << "Operatori: ";
+		std::cin >> a >> b;
+
+		std::cout << "Operacija: \n";
+		std::cout << "1. Sabiranje \n";
+		std::cout << "2. Oduzimaje \n";
+		std::cout << "3. Mnozenje \n";
+		std::cout << "4. Dijeljenje \n";
+		std::cout << "4. Dijeljenje \n";
+		int opcija;
+		std::cin >> opcija;
+		switch (opcija)
+		{
+		case 1: 
+			std::cout << add(a, b);
+			break;
+		case 2:
+			std::cout << subtract(a, b);
+			break;
+		case 3:
+			std::cout << multiply(a, b);
+			break;
+		case 4:
+			std::cout << divide(a, b);
+			break;
+		default:
+			break;
+		}
+	}
 	
 }
